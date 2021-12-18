70f4c6952f0f0e8468b70ac7d76952de59b029a0
Victor Verdun van der Watt <victor@Victors-MacBook-Pro.local>
2021-12-18 13:05:46 +0200

    Added use examples

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # ClassHound
+## Description
 Monitor and store class usage events and values.
+
+## Classes
+### classhound/monitor -> Monitor
+#### **Usage example:**
+
+ ```python
+from delta import *
+from classhound import Monitor
+
+monitor_table_writes = Monitor()
+readwriter.DataFrameWriter.save = monitor_table_writes.internal_function_parameter(
+    func=readwriter.DataFrameWriter.save,
+    param_details=(0, "path")
+)
+DeltaTable.forPath = monitor_table_writes.function_parameter(
+    func=DeltaTable.forPath,
+    param_details=(1, "path")
+    )
+
+df = spark.createDataFrame([(1, "John"), (2, "Bob")], ["id", "name"])
+df.write.format("delta").save("/path/table1", mode="append")
+...
+(
+    DeltaTable.forPath(spark, path="/path/table2").alias("dest")
+    .merge(
+        df.alias("src"),
+        condition="src.id = dest.id"
+    )
+)
+monitor_table_writes.values
+# ["/path/table1", "/path/table2"]
+```
+
+#### **Example of why would you use this?**
+Instead of manually searching/updating a list of dependencies (potentially missing something or making a mistake)
+as you develop. You can automate the updating of these lists / configs etc.
+
+This monitor class does not alter or break the actuall functionality of the functions it monitors. Allowing the
+program to work as intended and not getting in the way of development, having to remember to build out logic to caputer those values or having to do it manually.
+
+This is a set and forget if you configured all the functions that you are intersted in checking for those types of value updates or addons.
--- classhound/monitor.py
+++ classhound/monitor.py
@@ -2,31 +2,41 @@ from functools import wraps
 
 class Monitor:
     def __init__(self):
+        """ Store parameter values of a function when function is used in program.
+
+        Example:
+        m = Monitor()
+        print = m.function_parameter(print, (None, "end"))
+        print("Hi, Bob", end="\n\n")
+        print("Hi, Ron", end="\n")
+        print(m.values)
+        # ['\n\n', '\n']
+        """
         self.values = []
 
     @staticmethod
-    def _add_parameter_value(self, param_name, *args, **kwargs):
-        if len(args) > param_name[0]:
-            self.values.append(args[param_name[0]])
-        elif param_name[1] in kwargs:
-            self.values.append(kwargs[param_name[1]])
+    def _add_parameter_value(self, param_details, *args, **kwargs):
+        if param_details[0] and len(args) > param_details[0]:
+            self.values.append(args[param_details[0]])
+        elif param_details[1] in kwargs:
+            self.values.append(kwargs[param_details[1]])
 
-    def internal_function_parameter(self, func, param_name):
+    def internal_function_parameter(self, func, param_details):
         _self = self
         @wraps(func)
         def method(self, *args, **kwargs):
-            _self._add_parameter_value(_self, param_name, *args, **kwargs)
+            _self._add_parameter_value(_self, param_details, *args, **kwargs)
             return func(self, *args, **kwargs)
         return method
 
-    def function_parameter(self, func, param_name):
+    def function_parameter(self, func, param_details):
         @wraps(func)
         def method(*args, **kwargs):
-            self._add_parameter_value(self, param_name, *args, **kwargs)
+            self._add_parameter_value(self, param_details, *args, **kwargs)
             return func(*args, **kwargs)
         return method
 
-    def sniff_function_parameter(self, func, param_name):
+    def sniff_function_parameter(self, func, param_details):
         @wraps(func)
         def method(*args, **kwargs):
             has_self = False
@@ -35,7 +45,7 @@ class Monitor:
                 args = args[1:]
                 has_self = True
 
-            self._add_parameter_value(self, param_name, *args, **kwargs)
+            self._add_parameter_value(self, param_details, *args, **kwargs)
             if has_self:
                 return func(_self, *args, **kwargs)
             return func(*args, **kwargs)
