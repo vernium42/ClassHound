8d908883646c5509716150b7807518e86bd1447d
Vernium42 <vernium@icloud.com>
2021-12-23 22:33:29 +0200

    package renaming

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
-setuptools.setup(name='ClassHound',
+setuptools.setup(name='classhound',
       version='0.1',
       description='Monitor and store class usage events and values.',
-      url='https://github.com/vernium42/ClassHound',
+      url='https://github.com/vernium42/classhound',
       author='Vernium42',
       license='MIT',
       packages=setuptools.find_packages())
